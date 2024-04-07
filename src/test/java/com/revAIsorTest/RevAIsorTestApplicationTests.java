80b9c78da32fb8feb2250779dab0cd8b507ed56b
Julian David Valencia Restrepo <julianv302@gmail.com>
2024-04-06 22:46:45 -0500

    (Init): Init RevAIsor Spring Boot contact form app

---
+++ src/test/java/com/revAIsorTest/RevAIsorTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revAIsorTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevAIsorTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
